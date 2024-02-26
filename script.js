b7217aaa2995c82ce875f4f43f1fcc106d9fcf12
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-26 12:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  
+  if (str.length === 0) {
+    return null;
+  }
+
+ 
+  const charCount = new Map();
+
+  
+  for (const char of str) {
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+
+  
+  for (const char of str) {
+    if (charCount.get(char) === 1) {
+      return char;
+    }
+  }
+
+  
+  return null;
 }
+
+
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+
+
+alert(firstNonRepeatedChar(input));
+ 
